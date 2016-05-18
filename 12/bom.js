8cbcf63485e2c10a31e092bc770fa4e7146113dd
teann1996 <2273766743@qq.com>
2016-05-18 12:40:32 +0800

    memeda

--- 12/bom.js
+++ 12/bom.js
@@ -10,4 +10,34 @@ var person = {
 }
 
 console.log(person.name);
-person.sing();
+//person.sing();
+//for (var i in person) {
+	// console.log(i + ":" + person[i]);
+	//}
+
+	
+	for (var i in console) {
+		console.log (i + ":" + console[i]);
+	}
+	consle.clear();
+
+	console.log(typeof person);
+
+	//布尔类型
+
+	console.log(typeof false);
+	console.log(typeof 'false');
+
+	//数字类型
+	console.log(typeof 100);
+
+//字符串类型
+console.log(typeof person.name);
+
+
+//定义数组
+var sites = ['baidu.com', 'sohu.com', 'sina.com.cn', '163.com'];
+
+console.log(sites[0]);
+
+console.log(typeof sites);
